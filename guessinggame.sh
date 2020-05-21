abc322984b08d5a60d82c557ec7f01dbe9e3bc6e
ArtemVorobyev <artemvorobyev@mail.ru>
2020-05-21 17:31:20 +0200

    modified version of guessing game

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,26 @@
 echo "welcome in my quessing game!!!"
 
-filenumber=$(ls | wc -l)
+filenumber=$(ls -a -p | grep "/" -v | wc -l)
 
 echo "try to guess how many files are in the folder"
 
 function asking {
 	read response
 	
-if [[ $response -gt $filenumber ]]
-then
-	echo "you entered $response. It's way to much. Try again!"
-elif [[ $response -lt $filenumber ]]
-then
+	if [[ $response =~ [^0-9] ]]
+	then 
+	echo "the response should be a number"
+	
+	elif [[ $response -gt $filenumber ]]
+	then
+	echo "you entered $response. No, It should be less. Try again!"
+	elif [[ $response -lt $filenumber ]]
+	then
 	echo "you entered $response. No, it should be more. Try again!"
-elif [[ $response -eq $filenumber ]]
-then 
+	elif [[ $response -eq $filenumber ]]
+	then 
 	echo "Congratulations!!! Correct"	
-fi
+	fi
 }
 
 while [[ $response -ne $filenumber ]]
