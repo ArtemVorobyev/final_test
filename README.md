cfc8e18bf36ead5f4a787836cbd22d61d981b4b8
ArtemVorobyev <artemvorobyev@mail.ru>
2020-05-22 21:37:35 +0200

    updated version of guessinggame accepting variables starting with zeroes and new README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Final exercise for Coursera Unix Workbench class
-the file was run at  Thu May 21 22:25:24 CEST 2020;
-there are 30 lines in guessinggame.sh file
+the file was run at  Fri May 22 21:35:57 CEST 2020;
+there are 32 lines in guessinggame.sh file
--- guessinggame.sh
+++ guessinggame.sh
@@ -6,25 +6,27 @@ echo "try to guess how many files are in the folder"
 
 function asking {
 	read response
+
+	mod_response=$(echo $response | sed 's/^0*//')
 	
-	if [[ $response =~ [^0-9] ]]
+	if [[ $mod_response =~ [^0-9] ]]
 	then 
 	echo "it should be a number"
 	
-	elif [[ $response -gt $filenumber ]]
+	elif [[ $mod_response -gt $filenumber ]]
 	then
-	echo "you entered $response. No, It should be less. Try again!"
-	elif [[ $response -lt $filenumber ]]
+	echo "you entered $mod_response. No, It should be less. Try again!"
+	elif [[ $mod_response -lt $filenumber ]]
 	then
-	echo "you entered $response. No, it should be more. Try again!"
-	elif [[ $response -eq $filenumber ]]
+	echo "you entered $mod_response. No, it should be more. Try again!"
+	elif [[ $mod_response -eq $filenumber ]]
 	then 
 	echo "Congratulations!!! Correct"	
 	fi
 	
 }
 
-while [[ $response != $filenumber ]]
+while [[ $mod_response != $filenumber ]]
 do
 	asking
 done
